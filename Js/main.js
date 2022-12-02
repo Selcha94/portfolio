7ba10a684e9be1e4d0994576ed97cd39b377cefb
Selcha94 <selene.ch@hotmail.com>
2022-12-02 00:12:42 -0300

    ultimos cambios html

--- Js/main.js
+++ Js/main.js
@@ -2,3 +2,26 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
     menu.classList.toggle("move");
 }
+//animacion de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progress");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("mysql");
+        habilidades[4].classList.add("photoshop");
+        habilidades[5].classList.add("figma");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajoequipo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("autodidacta");
+        habilidades[10].classList.add("productividad")
+    }
+}
+    //detector de scrolling para detectar animaciones de la barra de habilidades
+    window.onscroll = function(){
+        efectoHabilidades();
+    }
